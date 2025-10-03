886a7953bf43fdfafd5fa1476ac8466c938fce03
Fabian Martin <fabianmartinanz@gmail.com>
2025-10-03 13:54:48 -0500

    feat(cartera): agrega wizard de creación de abono (4 pasos) y ruta /wallet/client/:id/abonos/new con botón de acceso desde facturas del cliente

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Dashboard from "./views/Dashboard";
 import Movements from "./views/Movements";
 import NewTransaction from "./views/NewTransaction";
 import Wallet from "./views/Wallet";
+import ClientInvoices from "./views/ClientInvoices";
+import CreateInvoiceWizard from "./views/CreateInvoiceWizard";
+import CreateAbonoWizard from "./views/CreateAbonoWizard";
+import InvoiceAbonos from "./views/InvoiceAbonos";
 import Reports from "./views/Reports";
 import Login from './views/Login';
 import { isAuthenticated, isAdmin } from './auth';
@@ -19,7 +23,11 @@ function App() {
       <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
       <Route path="/movements" element={<RequireAuth><Movements /></RequireAuth>} />
       <Route path="/new" element={<RequireAuth><NewTransaction /></RequireAuth>} />
-      <Route path="/wallet" element={<RequireAuth><Wallet /></RequireAuth>} />
+  <Route path="/wallet/*" element={<RequireAuth><Wallet /></RequireAuth>} />
+  <Route path="/wallet/client/:id/invoices" element={<RequireAuth><ClientInvoices /></RequireAuth>} />
+  <Route path="/wallet/client/:id/invoices/new" element={<RequireAuth><CreateInvoiceWizard /></RequireAuth>} />
+  <Route path="/wallet/client/:id/abonos/new" element={<RequireAuth><CreateAbonoWizard /></RequireAuth>} />
+  <Route path="/wallet/client/:clientId/invoices/:invoiceId/abonos" element={<RequireAuth><InvoiceAbonos /></RequireAuth>} />
   <Route path="/cashout" element={<RequireAuth><CashoutPOS /></RequireAuth>} />
       <Route path="/cashout-bank" element={<RequireAuth><BankWithdrawal /></RequireAuth>} />
       <Route path="/reports" element={<RequireAuth><Reports /></RequireAuth>} />
